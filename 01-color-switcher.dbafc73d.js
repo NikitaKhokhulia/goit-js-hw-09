65f88163065c8ef985d7f3abfd24dc7eb237ab71
NikitaKhokhulia <114909439+NikitaKhokhulia@users.noreply.github.com>
2022-12-30 13:55:28 +0000

    Deploying to gh-pages from @ NikitaKhokhulia/goit-js-hw-09@569e26bee89403f974e6373e6b2aa00f30c0e587 🚀

--- 01-color-switcher.dbafc73d.js
+++
@@ -1,2 +0,0 @@
-const t={bodyColor:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};function o(){t.bodyColor.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startBtn.addEventListener("click",(function(){timerId=setInterval(o,1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(timerId),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.dbafc73d.js.map
